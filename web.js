c8f48376c36f6cbc2ad52176a788e39d3c3d63f4
Adam Haris <iamadamharis@gmail.com>
2013-07-06 14:37:13 +0200

    set index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require('express'),
+  app = express.createServer(express.logger()),
+  http = require('http'),
+  fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write(fs.readFileSync("./index.html", "utf8").toString());
+  response.end();
 });
 
 var port = process.env.PORT || 5000;
